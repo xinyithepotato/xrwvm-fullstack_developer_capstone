4b436973c1b395bf0a4c8df80c28159bdc3bb0de
xinyithepotato <xinyi395@gmail.com>
2025-02-22 04:48:37 -0500

    push 5

--- server/database/app.js
+++ server/database/app.js
@@ -94,8 +94,8 @@ app.get('/fetchDealer/:id', async (req, res) => {
 //Express route to insert review
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
   data = JSON.parse(req.body);
-  const documents = await Reviews.find().sort( { id: -1 } )
-  let new_id = documents[0]['id']+1
+  const documents = await Reviews.find().sort( { id: -1 } );
+  let new_id = documents[0].id+1;
 
   const review = new Reviews({
 		"id": new_id,
