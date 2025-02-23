5ee54d77be6972354fbae0563cafca62bae356c7
xinyithepotato <xinyi395@gmail.com>
2025-02-23 03:16:56 -0500

    push

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -7,6 +7,8 @@ from django.views.decorators.csrf import csrf_exempt
 import logging
 import json
 from .restapis import get_request, analyze_review_sentiments, post_review
+from .models import CarMake, CarModel
+from .populate import initiate
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -126,3 +128,14 @@ def add_review(request):
                 })
 
     return JsonResponse({"status": 403, "message": "Unauthorized"})
+
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels":cars})
